63ed6ca57b4e970cc5c8d5c919bc00424b860090
Blake Embrey <hello@blakeembrey.com>
2014-02-03 21:13:46 -0500

    Thunkify every request method.

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-/*
-  Simple wrapper of request library
-  http://github.com/mikeal/request
-  For co-like interface.
-*/
-  
 var request = require('request');
+var __slice = Array.prototype.slice;
 
-module.exports = requestThunk;
+/**
+ * Thunkify a request method.
+ *
+ * @param  {Function} fn
+ * @return {Function}
+ */
+var thunkifyRequest = function (fn) {
+  return function () {
+    var args    = __slice.call(arguments);
+    var context = this;
 
-function requestThunk (opts) {
-  return function (done) {
-    request(opts, function (err, response, body) {
-      var result = response;
-      result.body = body;
-      done(err, result);
-    });
+    return function (done) {
+      // Push the callback manually to avoid additional arguments from co.
+      args.push(function (err, res) {
+        done(err, res);
+      });
+
+      return fn.apply(context, args);
+    };
   };
-}
+};
+
+/**
+ * Export the thunkified base request function.
+ *
+ * @type {Function}
+ */
+exports = module.exports = thunkifyRequest(request);
+
+// Regular request methods that don't need be thunkified.
+exports.jar    = request.jar;
+exports.cookie = request.cookie;
+
+// Attach all the regular request methods.
+['get', 'patch', 'post', 'put', 'head', 'del'].forEach(function (method) {
+  exports[method] = thunkifyRequest(request[method]);
+});
